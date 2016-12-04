6043b9ee34900e91eb2f93230825fb5b2be6ce12
paolo21d <paolo21d@gmail.com>
2016-12-04 16:18:08 +0100

    rozpoczęte pisanie Plansza

--- Tysiac/Tysiac/Plansza.cpp
+++ Tysiac/Tysiac/Plansza.cpp
@@ -1,1 +1,38 @@
 #include "includes.h"
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+Plansza::Plansza()
+{
+	//otworzenie pliku z zapisami
+	plik.open("dane_gry.txt");
+	if (!plik.good()) //nie udalo sie otworzyc pliku
+	{
+
+	}
+}
+Plansza::~Plansza()
+{
+	//zamkniecie pliku
+	plik.close();
+}
+int Plansza::get_ilosc_graczy()
+{
+	return ile_graczy;
+}
+void Plansza::tasuj()
+{
+
+}
+void Plansza::rozdaj(int ile)
+{
+
+}
+void Plansza::zapisz_gre()
+{
+
+}
+void Plansza::wczytaj_gre()
+{
+
+}
--- Tysiac/Tysiac/includes.h
+++ Tysiac/Tysiac/includes.h
@@ -4,18 +4,33 @@
 #include <string>
 #include <algorithm>
 #include <iostream>
-
+#include <vector>
+#include <fstream>
 class Karta
 {
-
+	int kolor;
+	int figura;
+	int id;
 };
 class Plansza
 {
-
+	int ile_graczy;
+	int meldunek;
+	Karta kar_na_gorze;
+	std::vector <Karta> karty_w_rece[4]; // najlepiej by bylo dynamicznie alokowac ten vector w tablice 
+	std::fstream plik;
+public: 
+	Plansza();
+	~Plansza(); //destruktor
+	int get_ilosc_graczy();
+	void tasuj();
+	void rozdaj(int ile);
+	void zapisz_gre();
+	void wczytaj_gre();
 };
 class Gracz
 {
-
+	std::vector <Karta> karty;
 };
 class Uzytkownik : public Gracz
 {
